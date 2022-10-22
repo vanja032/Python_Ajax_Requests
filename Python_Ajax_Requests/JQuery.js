d852ca932412861a25e31857c46f075931be3a3c
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2022-10-22 21:30:11 +0200

    Add files via upload

---
+++ Python_Ajax_Requests/JQuery.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function(){
+	
+	$("#H1").click(function(){
+		
+		$(function(){
+			$.ajax({
+				url: "localhost:9000/Hello",
+				type: "POST",
+				dataType: "text",
+				success: function(data){
+					console.log(data);
+				}
+			});
+		});
+	});
+	
+});
---
+++ Python_Ajax_Requests/python_server.py
@@ -0,0 +1,19 @@
+﻿from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS, cross_origin
+#import json
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return "A"
+
+@app.route('/Hello', methods=['GET','POST'])
+def Hello():
+    data = "Hello, Hello, World!"
+    data.headers['Access-Control-Allow-Origin'] = '*'
+    return data
+
+if __name__ == "__main__":
+	app.run(debug=True)
